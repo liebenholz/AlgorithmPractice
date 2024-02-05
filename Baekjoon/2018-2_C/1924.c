99f1d1bbefa1c1ba3208d6eb38ef97b918135760
Seonghyeok Jo <liebenholz98@gmail.com>
2024-02-05 16:19:12 +0900

    Initiate

--- Baekjoon/2018-2_C/1924.c
+++
@@ -1,9 +0,0 @@
-int main() {
-    int m, d;
-    int month[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}; 
-    char *day[] = { "SUN" ,"MON", "TUE", "WED", "THU", "FRI", "SAT" };
-    scanf("%d %d", &m, &d);
-    for(int i = 0; i < m - 1; i++)
-        d += month[i];
-    printf("%s", day[d%7]);
-}
